e7666c9b371001e4cca63e82321eec460ec69658
jlipes <jason.lipes@gmail.com>
2015-07-29 20:26:59 -0500

    finished

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,23 @@ Rails.application.routes.draw do
   get("/", { :controller => "photos", :action => "index" })
 
   # Routes to READ photos
+  #Create
+  get("/photos/new",       { :controller => "photos", :action => "create_row_form" })
+  get("/photo/create_photo/",       { :controller => "photos", :action => "create_row" })
+
+  #Read
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+  #Update
+  get("/photo/update_photo/:id",       { :controller => "photos", :action => "update_row" })
+  get("/photos/:id/edit",       { :controller => "photos", :action => "edit_form" })
+
+  #Delete
+    get("/delete_photo/:id",       { :controller => "photos", :action => "destroy" })
+
+
+
+
 
 end
